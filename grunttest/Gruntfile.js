3b2289cd9c11b480b073e214393020a58f431f3e
FrankLiu <tonyliu2005@gmail.com>
2014-06-03 22:30:15 +0800

    update Gruntfile.js

--- grunttest/Gruntfile.js
+++ grunttest/Gruntfile.js
@@ -3,20 +3,59 @@ module.exports = function(grunt){
 	//Project configuration
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['src/**/*.js'],
+				dest: 'dist/<%=pkg.name%>.js'
+			}
+		},
+		
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
 			},
-			build: {
-				src: 'src/<%=pkg.name%>.js',
-				dest: 'build/<%=pkg.name%>.min.js'
+			dist: {
+				files: {
+					'dist/<%=pkg.name%>.min.js': ['<%=concat.dist.dest%>']
+				}
 			}
+		},
+		
+		qunit: {
+			files: ['test/**/*.html']
+		},
+		
+		jshint: {
+			files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+			options: {
+				globals: {
+				  jQuery: true,
+				  console: true,
+				  module: true,
+				  document: true
+				}
+			}
+		},
+		
+		watch: {
+			files: ['<%=jshint.files%>'],
+			tasks: ['jshint', 'qunit']
 		}
 	});
 	
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+  
+	grunt.registerTask('test', ['jshint', 'qunit']);
+	grunt.registerTask('test2', ['concat', 'uglify']);
 	
 	//Default task(s).
-	grunt.registerTask('default', ['uglify']);
+	grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
 };
---
+++ grunttest/dist/grunttest.js
@@ -0,0 +1,6 @@
+var util = require('util');
+
+util.inspect(console.log);
+util.log("this is a test script for grunt!");
+
+;console.log("foo");

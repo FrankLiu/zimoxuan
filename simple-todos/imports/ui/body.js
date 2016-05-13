703bdaa3a151e05ff18773a8de400a02a9f19ae2
Frank Liu <tonyliu2005@gmail.com>
2016-05-13 11:23:18 +0800

    update simple-todos meteor app

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
@@ -7,6 +8,7 @@ import './body.html';
  
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
 });
 
 Template.body.helpers({
@@ -33,10 +35,7 @@ Template.body.events({
     const text = target.text.value;
  
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
+    Meteor.call('tasks.insert', text);
  
     // Clear form
     target.text.value = '';
--- simple-todos/imports/ui/tasks.js
+++ simple-todos/imports/ui/tasks.js
@@ -4,15 +4,23 @@ import { Tasks } from '../api/tasks.js';
  
 import './tasks.html';
  
+ Template.task.helpers({
+	isOwner() {
+		return this.owner === Meteor.userId();
+	}, 
+ });
+ 
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {
-      $set: { checked: ! this.checked },
-    });
+    Meteor.call('tasks.setChecked', this._id, !this.checked);
   },
   
   'click .delete'() {
-    Tasks.remove(this._id);
+    Meteor.call('tasks.remove', this._id);
+  },
+  
+  'click .toggle-private'() {
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
   },
 });

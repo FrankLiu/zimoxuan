ba3da6ee17fe88c6d64f7c7f0d1e7097c63002e6
FrankLiu <tonyliu2005@gmail.com>
2016-06-04 22:01:50 +0800

    add ServerSendEvent test

---
+++ test/h5/ServerSendEvent/sse_datepush.js
@@ -0,0 +1,12 @@
+//implements server send event
+
+var http = require("http");
+http.createServer(function(request, response){
+  response.writeHead(200, { "Content-Type": "text/event-stream" });
+  setInterval(function(){
+    var content = "data:" +
+      new Date().toISOString() + "\n\n";
+    response.write(content);
+    }, 1000);
+  }).listen(1234);
+

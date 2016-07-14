ecb382c465ca8b86c82c468854777e22ee678c72
FrankLiu <tonyliu2005@gmail.com>
2016-07-14 17:30:17 +0800

    add zk_test

--- test/zk_test.js
+++ test/zk_test.js
@@ -1,21 +1,48 @@
-var zookeeper  = require('node-zookeeper-client');
-
-var client = zookeeper.createClient('192.168.7.7:2181');
+var zookeeper = require('node-zookeeper-client');
+ 
+ var host = '192.168.7.7:2181';
+var client = zookeeper.createClient(host);
 var path = process.argv[2];
 
-client.once('connnected', function(){
-	console.log('connected to the server 192.168.7.7:2181');
+function createPath(path){
+    client.create(path, function (error) {
+        if (error) {
+            console.log('Failed to create node: %s due to: %s.', path, error);
+        } else {
+            console.log('Node: %s is successfully created.', path);
+        }
+ 
+        client.close();
+    });
+}
 
-	client.create(path, function(err){
-		if(err){
-		   console.log('failed to create node: %s due to: %s.', path, err);	
-		} else { 
-		   console.log('Node: %s is successfully created.', path);
-		}
-	
-		client.close();
-	});
+function listChildren(path){
+    client.getChildren(
+        path,
+        function (event) {
+            console.log('Got watcher event: %s', event);
+            listChildren(client, path);
+        },
+        function (error, children, stat) {
+            if (error) {
+                console.log(
+                    'Failed to list children of %s due to: %s.',
+                    path,
+                    error
+                );
+                return;
+            }
+ 
+            console.log('Children of %s are: %j.', path, children);
+        }
+    );
+}
+ 
+client.once('connected', function () {
+    console.log('Connected to the zookeeper server: %s', host);
+ 
+    //createPath(path);
+    listChildren(path);
 });
-
+ 
 client.connect();
-

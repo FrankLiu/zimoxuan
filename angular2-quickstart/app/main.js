8f72d374889725f914b93f3845159bebed4c6d93
liujun <liujun@liujundeMacBook-Pro.local>
2016-09-29 06:35:42 +0800

    update angular2-quickstart based on angular2.0

--- angular2-quickstart/app/main.js
+++ angular2-quickstart/app/main.js
@@ -1,5 +1,7 @@
-(function(app){
-	document.addEventListener('DOMContentLoaded', function(evt){
-		ng.platform.browser.bootstrap(app.AppComponent);
-	});
+(function(app) {
+  document.addEventListener('DOMContentLoaded', function() {
+    ng.platformBrowserDynamic
+      .platformBrowserDynamic()
+      .bootstrapModule(app.AppModule);
+  });
 })(window.app || (window.app = {}));

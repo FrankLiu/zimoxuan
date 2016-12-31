2f8400694e3342c8d1c2b16a79706b8f04508ece
liujun <tonyliu2006@gmail.com>
2016-12-31 09:53:36 +0800

    update

--- simple-todos/vuejs-project/src/App.vue
+++ simple-todos/vuejs-project/src/App.vue
@@ -2,18 +2,37 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <div id="data-table-demo"></div>
   </div>
 </template>
 
 <script>
 import Hello from './components/Hello'
+import DataTable from './components/DataTable'
+
+// bootstrap the demo
+var demo = new Vue({
+  el: '#demo',
+  data: {
+    searchQuery: '',
+    gridColumns: ['name', 'power'],
+    gridData: [
+      { name: 'Chuck Norris', power: Infinity },
+      { name: 'Bruce Lee', power: 9000 },
+      { name: 'Jackie Chan', power: 7000 },
+      { name: 'Jet Li', power: 8000 }
+    ]
+  }
+})
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Hello,
+    DataTable
   }
 }
+
 </script>
 
 <style>

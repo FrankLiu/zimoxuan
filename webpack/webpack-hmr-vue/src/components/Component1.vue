29be97af9ddf771c2ef45bf938ac7df925d64bbc
liujun <liujun@jianglaibao.cn>
2017-02-20 09:24:55 +0800

    add webpack-hmr-vue project

---
+++ webpack/webpack-hmr-vue/src/components/Component1.vue
@@ -0,0 +1,12 @@
+<script>
+import Vue from 'vue';
+
+export default {
+  name: 'Component1',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
---
+++ webpack/webpack-hmr-vue/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Component1 from './components/Component1.vue';
+
+new Vue({
+  components: {
+    Component1
+  }
+}).$mount('#content')

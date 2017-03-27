f9ad666da7edd8d407e5e99a30a4746550c0b02f
liujun <liujun@jianglaibao.cn>
2017-03-27 16:07:49 +0800

    combine the DataBinder_withNative & DataBinder_withJQuery

--- frontend/mvvm/mymvvm/data-binder.js
+++ frontend/mvvm/mymvvm/data-binder.js
@@ -14,22 +14,22 @@ function DataBinder_withJQuery(object_id){
 		var pubSub = jQuery({});
 		
 		//我们希望一个data元素可以在表单中指明绑定：data-bind-<object_id>="<property_name>"  
-		var data_attr = "bind-"+object_id,
+		var data_attr = "data-bind-"+object_id,
 				message = object_id + ":change";
 		
 		//使用data-binding属性和代理来监听那个元素上的变化事件
 		// 以便变化能够“广播”到所有的关联对象   
-		jQuery(document).on("change", "[data-"+data_attr+"]", function(evt){
+		jQuery(document).on("change", "["+data_attr+"]", function(evt){
 			var $input = jQuery(this);
-			pubSub.trigger(message, [$input.data(data_attr), $input.val()]);
+			pubSub.trigger(message, [$input.attr(data_attr), $input.val()]);
 		});
 		
 		//PubSub将变化传播到所有的绑定元素，设置input标签的值或者其他标签的HTML内容   
 		pubSub.on(message, function(evt, prop_name, new_val){
-			jQuery('[data-'+data_attr+'='+prop_name+']').each(function(){
+			jQuery('['+data_attr+'='+prop_name+']').each(function(){
 				var $bound = jQuery(this);
 				
-				if($bound.is("input,textarea,select")){
+				if($bound.is("input, textarea, select")){
 					$bound.val(new_val);
 				} else {
 					$bound.html(new_val);
@@ -39,7 +39,11 @@ function DataBinder_withJQuery(object_id){
 		
 		//align to native implement
 		pubSub.subscribe = pubSub.on;
-		pubSub.publish = pubSub.trigger;
+		pubSub.publish = function(){
+			if(arguments.length < 1) return;
+			var args = [].slice.call(arguments,1);
+			pubSub.trigger(arguments[0], args);
+		};
 		
 		return pubSub;
 	}
